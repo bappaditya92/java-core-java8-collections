8615e8de4c4d39e05382f552bc8d029110850539
bappaditya92 <bappadityasinha.92@gmail.com>
2026-03-04 23:19:27 +0530

    Added first core java program

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello GitHub");
+        System.out.println("Core Java Practice");
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = " + (a + b));
+    }
+
+}
