c082ef1520ba95466ff26470e6437b361d71ebf7
bappaditya92 <bappadityasinha.92@gmail.com>
2026-03-07 21:43:49 +0530

    add Queue DSA problem

--- src/TwoDimensionalArray.java
+++ src/TwoDimensionalArray.java
@@ -25,7 +25,6 @@ public class TwoDimensionalArray {
             System.out.println("Invalid index for 2D array");
         }
     }
-
     // Accessing cell value from given array
     public void accessCell(int row, int col) {
         System.out.println("\nAccessing Row#" + row + ", Col#" + col);
