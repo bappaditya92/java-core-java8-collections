15c6dff56b74f527160562ffc3c53e0ee9d2d654
bappaditya92 <bappadityasinha.92@gmail.com>
2026-03-18 20:55:16 +0530

    Add factorial code without recursion

---
+++ src/RecursionExample.java
@@ -0,0 +1,13 @@
+public class RecursionExample {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+
+    }
+    public static int factorial(int n) {
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+        }
+        return res;
+    }
+}
