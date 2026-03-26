600e14b7b5fa739b8a025b64903a0380711dd1e4
bappaditya92 <bappadityasinha.92@gmail.com>
2026-03-26 14:55:20 +0530

    Remove extra spaces and comment

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -6,25 +6,15 @@ public class ArrayListExample {
 
         ArrayList<String> fruits = new ArrayList<>();
 
-        // Adding elements
         fruits.add("Apple");
         fruits.add("Mango");
         fruits.add("Banana");
         fruits.add("Orange");
 
-        // Printing the list
         System.out.println("Fruit List: " + fruits);
-
-        // Access element
         System.out.println("First fruit: " + fruits.get(0));
-
-        // Remove element
         fruits.remove("Mango");
-
-        // Size of list
         System.out.println("Total fruits: " + fruits.size());
-
-        // Loop through list
         System.out.println("All fruits:");
         for(String fruit : fruits){
             System.out.println(fruit);
