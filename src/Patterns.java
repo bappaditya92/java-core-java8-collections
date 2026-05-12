17a30c8a0a40c63d7344173153dbeba55fa988fe
bappaditya92 <bappadityasinha.92@gmail.com>
2026-05-12 17:13:51 +0530

    Add some code

--- src/Patterns.java
+++ src/Patterns.java
@@ -29,6 +29,8 @@ public class Patterns {
         }
     }
 
+    //add this code into repository
+
     public static void printPattern4(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
