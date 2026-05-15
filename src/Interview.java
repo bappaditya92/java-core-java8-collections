c1c8e1f7df2024bdfa1f095cf663fb872b6059ff
bappaditya92 <bappadityasinha.92@gmail.com>
2026-05-15 12:10:06 +0530

    Add a interview question asked in Wipro interview

---
+++ src/Interview.java
@@ -0,0 +1,17 @@
+public class Interview {
+    public static void main(String[] args) {
+
+        String input = "Java";
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            output.append(input.charAt(i));
+            for (int j = 0; j <= i; j++) {
+                output.append("#");
+            }
+        }
+
+        System.out.println(output);
+    }
+}
