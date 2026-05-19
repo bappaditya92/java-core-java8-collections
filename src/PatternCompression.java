f6f55ab9ba630e55be809519ebd7dc3af58d41d8
bappaditya92 <bappadityasinha.92@gmail.com>
2026-05-19 16:19:28 +0530

    Add pattern compression problem of leetcode

---
+++ src/PatternCompression.java
@@ -0,0 +1,23 @@
+public class PatternCompression {
+    public static void main(String[] args) {
+
+        String str = "xxxyyyzzzzzzz";
+        StringBuilder result = new StringBuilder();
+
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i < str.length() - 1 &&
+                    str.charAt(i) == str.charAt(i + 1)) {
+
+                count++;
+            } else {
+                result.append(str.charAt(i)).append(count);
+                count = 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
