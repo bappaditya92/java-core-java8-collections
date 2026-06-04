c8978388067bbd726bfcd09d341a1b260e77e71a
bappaditya92 <bappadityasinha.92@gmail.com>
2026-06-04 19:20:55 +0530

    refactor: move all code into leetcode package

---
+++ src/leetcode/GenericsExample.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class GenericsExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+}
