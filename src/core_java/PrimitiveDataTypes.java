13341eabb86ffa1697d4ef9241dda2e79bc990ef
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-01 12:38:39 +0530

    Initial commit of primitive data type codes for practices

---
+++ src/core_java/PrimitiveDataTypes.java
@@ -0,0 +1,7 @@
+package core_java;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        System.out.println("This is the test code primitive data type practice code");
+    }
+}
