6b8416e02bc047aebcf4c092946417d2bc3dd240
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-30 12:09:13 +0530

    add core java practice code

--- src/core_java/Arthmetic.java
+++ src/core_java/Arthmetic.java
@@ -6,8 +6,14 @@ public class Arthmetic {
 //        int bonus = 500;
 //        int totalSalary = salary + bonus;
 //        System.out.println("salary is: " +totalSalary);
-        int deduction = 200;
-        int monthlySalary = salary - deduction;
-        System.out.println("monthly salary:" + monthlySalary);
+//        int deduction = 200;
+//        int monthlySalary = salary - deduction;
+//        System.out.println("monthly salary:" + monthlySalary);
+
+        int a = 5;
+        int b = 3;
+        int reminder = a%b;
+        System.out.println(reminder);
+
     }
 }
