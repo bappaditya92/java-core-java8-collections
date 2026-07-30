a380f5292fbe0214ef1d6f3d47ac5216883a03ca
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-30 12:06:46 +0530

    add core java practice code

--- src/core_java/Arthmetic.java
+++ src/core_java/Arthmetic.java
@@ -3,8 +3,11 @@ package core_java;
 public class Arthmetic {
     public static void main(String[] args) {
         int salary = 1000;
-        int bonus = 500;
-        int totalSalary = salary + bonus;
-        System.out.println("salary is: " +totalSalary);
+//        int bonus = 500;
+//        int totalSalary = salary + bonus;
+//        System.out.println("salary is: " +totalSalary);
+        int deduction = 200;
+        int monthlySalary = salary - deduction;
+        System.out.println("monthly salary:" + monthlySalary);
     }
 }
