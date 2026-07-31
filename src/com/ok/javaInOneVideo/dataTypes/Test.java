744925f134c60b8b770158a946ac3c12643aea14
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-31 19:21:04 +0530

    add basic code

--- src/com/ok/javaInOneVideo/dataTypes/Test.java
+++ src/com/ok/javaInOneVideo/dataTypes/Test.java
@@ -2,6 +2,15 @@ package com.ok.javaInOneVideo.dataTypes;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.printf("OK...");
+        //System.out.printf("OK...");
+
+        int a = 1;
+        byte b = 2;
+        short c = 3;
+        long d = 4;
+
+        System.out.println(c+d);
+
+
     }
 }
