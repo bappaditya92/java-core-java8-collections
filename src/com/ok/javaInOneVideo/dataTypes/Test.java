d32338426f63747e4daac5103934c7e0a9ddf349
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-31 22:46:04 +0530

    add basic code

--- src/com/ok/javaInOneVideo/dataTypes/Test.java
+++ src/com/ok/javaInOneVideo/dataTypes/Test.java
@@ -1,5 +1,7 @@
 package com.ok.javaInOneVideo.dataTypes;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Test {
     public static void main(String[] args) {
         //System.out.printf("OK...");
@@ -14,8 +16,17 @@ public class Test {
 //        //System.out.println("Byte Range"+Byte.MIN_VALUE+Byte.MAX_VALUE);
 //        System.out.println("Long range: "+Long.MAX_VALUE);
 
-        boolean isAdult = true;
-        isAdult = false;
-        System.out.println(isAdult);
+//        boolean isAdult = true;
+//        isAdult = false;
+//        System.out.println(isAdult);
+        char b = 'b';
+
+        System.out.println((int) b);
+
+        System.out.println("Minimum Val: " + (int) Character.MIN_VALUE);
+        System.out.println("maximum val: " + (int) Character.MAX_VALUE);
+//common ASCII example (0-127 range)
+        System.out.println((char) 10084);
+
     }
 }
