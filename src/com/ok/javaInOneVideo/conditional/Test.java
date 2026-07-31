d394024755ea88fd374239e59005c011ff06888c
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-31 23:49:09 +0530

    add basic code

--- src/com/ok/javaInOneVideo/conditional/Test.java
+++ src/com/ok/javaInOneVideo/conditional/Test.java
@@ -4,9 +4,18 @@ public class Test {
     public static void main(String[] args) {
 //        System.out.println(1>2 && 1!=2);
 //        System.out.println(1>2 || 1!=2);
+//        boolean a = true;
+//        System.out.println(!a);
 
-        boolean a = true;
-        System.out.println(!a);
+        boolean isSunny = true;
+        boolean isWarm = true;
+
+        if(isSunny || isWarm){
+            System.out.println("Go to outside...");
+        }
+        else{
+            System.out.println("Ghar me raho vai...");
+        }
 
     }
 }
