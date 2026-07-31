48d127d3e43ae7e251b3560ae6516fd83199d70f
bappaditya92 <bappadityasinha.92@gmail.com>
2026-08-01 00:06:16 +0530

    add basic code

--- src/com/ok/javaInOneVideo/conditional/Test.java
+++ src/com/ok/javaInOneVideo/conditional/Test.java
@@ -6,17 +6,67 @@ public class Test {
 //        System.out.println(1>2 || 1!=2);
 //        boolean a = true;
 //        System.out.println(!a);
+//
+//        boolean isSunny = true;
+//        boolean isWarm = false;
+//        if(isSunny || isWarm){
+//            System.out.println("Go to outside...");
+//        } else if (isSunny) {
+//            System.out.println("Ghar theke bero vai");
+//        } else{
+//            System.out.println("Ghar me raho vai...");
+//        }
 
-        boolean isSunny = true;
-        boolean isWarm = false;
-        if(isSunny || isWarm){
-            System.out.println("Go to outside...");
-        } else if (isSunny) {
-            System.out.println("Ghar theke bero vai");
 
-        } else{
-            System.out.println("Ghar me raho vai...");
+    int day = 4;
+    String dayName;
+
+//    if(day ==1){
+//        dayName="Monday";
+//    } else if (day == 2) {
+//        dayName = "Tuesday";
+//    } else if (day==3) {
+//        dayName = "Wednesday";
+//    } else if (day==4) {
+//        dayName = "Thursday";
+//    } else if (day==5) {
+//        dayName = "Friday";
+//    } else if (day==6){
+//        dayName = "Saturday";
+//        } else if (day==7){
+//        dayName = "Sunday";
+//    }
+//    else {
+//        dayName = "Invalid Day";
+//    }
+//        System.out.println(dayName);
+
+        switch(day){
+            case 1: dayName = "Monday";
+            break;
+
+            case 2: dayName = "Tuesday";
+            break;
+
+            case 3: dayName = "Wednesday";
+            break;
+
+            case 4: dayName = "Thursday";
+            break;
+
+            case 5: dayName = "Friday";
+            break;
+
+            case 6: dayName = "Saturday";
+            break;
+
+            case 7: dayName = "Sunday";
+            break;
+
+            default: dayName = "Invalid Day";
         }
+        System.out.println(day);
+
 
     }
 }
