acee0b66aaf328ad01752f54df8da918dfd80e4b
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-31 22:40:01 +0530

    add basic code

--- src/com/ok/javaInOneVideo/dataTypes/Test.java
+++ src/com/ok/javaInOneVideo/dataTypes/Test.java
@@ -4,14 +4,18 @@ public class Test {
     public static void main(String[] args) {
         //System.out.printf("OK...");
 
-        int a = 1;
-        byte b = 2;
-        short c = 3;
-        long d = 4;
+//        int a = 1;
+//        byte b = 2;
+//        short c = 3;
+//        long d = 9223372036854775807l;
+//
+//        System.out.println(d+c);
+//
+//        //System.out.println("Byte Range"+Byte.MIN_VALUE+Byte.MAX_VALUE);
+//        System.out.println("Long range: "+Long.MAX_VALUE);
 
-        System.out.println(c+d);
-
-        System.out.println("Byte Range"+Byte.MIN_VALUE+Byte.MAX_VALUE);
-
+        boolean isAdult = true;
+        isAdult = false;
+        System.out.println(isAdult);
     }
 }
