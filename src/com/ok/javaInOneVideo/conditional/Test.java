e383249482674a52cbc7bc53fcb8c4e0b03d0aa5
bappaditya92 <bappadityasinha.92@gmail.com>
2026-07-31 23:44:48 +0530

    add basic code

---
+++ src/com/ok/javaInOneVideo/conditional/Test.java
@@ -0,0 +1,12 @@
+package com.ok.javaInOneVideo.conditional;
+
+public class Test {
+    public static void main(String[] args) {
+//        System.out.println(1>2 && 1!=2);
+//        System.out.println(1>2 || 1!=2);
+
+        boolean a = true;
+        System.out.println(!a);
+
+    }
+}
