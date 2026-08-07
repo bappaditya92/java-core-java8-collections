25c5672a9e834ac2cd735ebbcb15b701609de6c6
bappaditya92 <bappadityasinha.92@gmail.com>
2026-08-07 20:17:27 +0530

    add basic array code

--- src/com/ok/javaInOneVideo/arrays/Test.java
+++ src/com/ok/javaInOneVideo/arrays/Test.java
@@ -3,12 +3,19 @@ package com.ok.javaInOneVideo.arrays;
 public class Test {
 
     public static void main(String[] args) {
-        int[] arr = new int[20];
-        arr[3] = 56;
-        System.out.println(arr[3]);
-        for(int i =0; i<arr.length; i++){
+//            int[] arr = new int[20];
+//        arr[3] = 56;
+//        System.out.println(arr[3]);
+//        for(int i =0; i<arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+
+
+        int[] arr = {1,2,5,6,3,7,9};
+        for (int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
         }
+        //System.out.println(arr);
 
     }
 
