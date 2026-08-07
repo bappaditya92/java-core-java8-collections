d3b34420ac440f8971da792ca4f2d68a3bc9c815
bappaditya92 <bappadityasinha.92@gmail.com>
2026-08-07 20:36:44 +0530

    add basic array code

--- src/com/ok/javaInOneVideo/arrays/Test.java
+++ src/com/ok/javaInOneVideo/arrays/Test.java
@@ -17,10 +17,44 @@ public class Test {
 //        }
 //        //System.out.println(arr);
 
-     int[] arr1 = {4,5,8,59,9,47,8,661,87,92,46};
-     for(int i: arr1){
-         System.out.println(i);
-     }
+//     int[] arr1 = {4,5,8,59,9,47,8,661,87,92,46};
+//     for(int i: arr1){
+//         System.out.println(i);
+//     }
+
+
+ // Search an element in an array
+//        int arr[] = {9,47,8,661,87,92};
+//        for(int i: arr){
+//            if(i==661){
+//                System.out.println("Found");
+//            }
+//            else{
+//                System.out.println("not found");
+//            }
+//        }
+
+
+
+        //Find Maximum element
+
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2000;
+        arr[2] = 855;
+        arr[3] = -4;
+        arr[4] = 85;
+        int res = Integer.MIN_VALUE;
+
+       for (int i=0; i<arr.length; i++){
+           if(arr[i]>res){
+               res = arr[i];
+           }
+
+       }
+        System.out.println(res);
+
+
 
     }
 
