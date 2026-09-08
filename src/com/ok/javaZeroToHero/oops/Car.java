7d0801b247757aa42973733ba245718778daa2af
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-08 14:07:57 +0530

    inital commit of oops

---
+++ src/com/ok/javaZeroToHero/oops/Car.java
@@ -0,0 +1,18 @@
+package com.ok.javaZeroToHero.oops;
+
+public class Car {
+    String color;
+    String brand;
+    String model;
+    int year;
+    int speed;
+
+    public void accelerate(int increment){
+        speed += increment;
+}
+
+
+}
+
+
+
--- src/com/ok/javaZeroToHero/oops/Test.java
+++
@@ -1,8 +0,0 @@
-package com.ok.javaZeroToHero.oops;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello from oops");
-    }
-}
-
