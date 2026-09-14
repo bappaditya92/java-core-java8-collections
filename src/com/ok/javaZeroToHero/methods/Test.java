6501017d241d54f8c3704bf9dff4bfd89a1f30fe
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-14 12:16:10 +0530

    Inital commit of methods

--- src/com/ok/javaZeroToHero/methods/Test.java
+++ src/com/ok/javaZeroToHero/methods/Test.java
@@ -3,11 +3,13 @@ package com.ok.javaZeroToHero.methods;
 public class Test {
 
     public static void main(String[] args) {
-        addNum(10, 12);
-    }
+//        addNum(10, 12);
+//    }
+//
+//    public static void addNum(int a, int b) {
+//        int c = a + b;
+//        System.out.println("sum of two num: " + c);
 
-    public static void addNum(int a, int b) {
-        int c = a + b;
-        System.out.println("sum of two num: " + c);
+
     }
 }
