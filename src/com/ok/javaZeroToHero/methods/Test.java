4acdb5526eb7f247248113644f81deb3b9bd69f2
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-15 17:50:15 +0530

    Add some code

--- src/com/ok/javaZeroToHero/methods/Test.java
+++ src/com/ok/javaZeroToHero/methods/Test.java
@@ -3,12 +3,12 @@ package com.ok.javaZeroToHero.methods;
 public class Test {
 
     public static void main(String[] args) {
-//        addNum(10, 12);
-//    }
-//
-//    public static void addNum(int a, int b) {
-//        int c = a + b;
-//        System.out.println("sum of two num: " + c);
+        addNum(10, 12);
+    }
+
+    public static void addNum(int a, int b) {
+        int c = a + b;
+        System.out.println("sum of two num: " + c);
 
 
     }
