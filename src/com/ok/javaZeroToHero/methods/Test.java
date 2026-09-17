3cccd4e01367d9dc755b01f5f1bd14577e19a12d
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 20:30:44 +0530

    Add some code

--- src/com/ok/javaZeroToHero/methods/Test.java
+++ src/com/ok/javaZeroToHero/methods/Test.java
@@ -3,22 +3,34 @@ package com.ok.javaZeroToHero.methods;
 public class Test {
 
     public static void main(String[] args) {
-        int[] a = {10,52,1,2,3,6,9,8};
-        sumOfArray(a);
+//        int[] a = {10,52,1,2,3,6,9,8};
+//        sumOfArray(a);
+//        int z = sumOfArray(a);
+//        System.out.println(z);
+
+        String str1 = upperFunc("bappaditya");
+        System.out.println(str1);
     }
 
 //    public static void addNum(int a, int b) {
 //        int c = a + b;
 //        System.out.println("sum of two num: " + c);
 
-    public static void sumOfArray(int[] arr) {
-        //int arr[] = {1,2,3,4,5};
-        int res = 0;
-        for (int i : arr) {
-            res += i;
-        }
-        System.out.println(res);
+//    public static int sumOfArray(int[] arr) {
+//        //int arr[] = {1,2,3,4,5};
+//        int res = 0;
+//        for (int i : arr) {
+//            res += i;
+//        }
+//        //System.out.println(res);
+//        return res;
+    //}
+    private static String upperFunc(String str){
+       return str.trim().toUpperCase();
+
 
     }
+
+
     }
 
