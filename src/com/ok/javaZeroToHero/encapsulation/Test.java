0de62e27a3f471fb489e3762172a755b815f4f64
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 23:07:19 +0530

    Add encapsulations code

--- src/com/ok/javaZeroToHero/encapsulation/Test.java
+++ src/com/ok/javaZeroToHero/encapsulation/Test.java
@@ -5,11 +5,14 @@ public class Test {
 
         Account account = new Account();
 
+        account.setAccountNumber(22110101);
         account.deposit(-10);
         account.withdraw(1000);
         account.deposit(1000);
         account.withdraw(105);
         System.out.println(account.getBalance());
+        System.out.println(account.getAccountNumber());
+
 
 
 
