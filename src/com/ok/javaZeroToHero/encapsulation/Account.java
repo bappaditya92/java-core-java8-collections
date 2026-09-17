302edb4eb58be9885116b75df676ce93bcfb55d4
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 23:05:37 +0530

    Add encapsulations code

---
+++ src/com/ok/javaZeroToHero/encapsulation/Account.java
@@ -0,0 +1,44 @@
+package com.ok.javaZeroToHero.encapsulation;
+
+public class Account {
+    private long accountNumber;
+    private double balance;
+
+    public void deposit(double amount){
+        if(amount > 0){
+           balance += amount;
+            System.out.println("deposited " + amount);
+        }
+        else {
+            System.out.println("Invalid deposit amount");
+        }
+
+    }
+    public void withdraw(double amount){
+        if(amount > 0 && amount <= balance){
+            balance -=amount;
+            System.out.println("withdrawn " + amount);
+        }
+        else {
+            System.out.println("Invalid withdrawal amount or insufficient balance");
+        }
+
+
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- src/com/ok/javaZeroToHero/encapsulation/Test.java
+++ src/com/ok/javaZeroToHero/encapsulation/Test.java
@@ -2,13 +2,16 @@ package com.ok.javaZeroToHero.encapsulation;
 
 public class Test {
     public static void main(String[] args) {
-    Student student = new Student();
 
-    student.name = "Bappaditya";
-    student.setAge(15);
-    student.rollNo = 1;
-        System.out.println(student.name);
-        System.out.println(student.getAge());
+        Account account = new Account();
+
+        account.deposit(-10);
+        account.withdraw(1000);
+        account.deposit(1000);
+        account.withdraw(105);
+        System.out.println(account.getBalance());
+
+
 
 
     }
