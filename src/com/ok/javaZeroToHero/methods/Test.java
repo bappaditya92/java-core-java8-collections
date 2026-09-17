c38a888d4f5bf1e0d8bb35751c7bd11e0644fe34
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 20:24:18 +0530

    Add some code

--- src/com/ok/javaZeroToHero/methods/Test.java
+++ src/com/ok/javaZeroToHero/methods/Test.java
@@ -3,21 +3,22 @@ package com.ok.javaZeroToHero.methods;
 public class Test {
 
     public static void main(String[] args) {
-        sumOfArray();
+        int[] a = {10,52,1,2,3,6,9,8};
+        sumOfArray(a);
     }
 
 //    public static void addNum(int a, int b) {
 //        int c = a + b;
 //        System.out.println("sum of two num: " + c);
 
-    public static int sumOfArray() {
-        int arr[] = {1,2,3,4,5};
+    public static void sumOfArray(int[] arr) {
+        //int arr[] = {1,2,3,4,5};
         int res = 0;
         for (int i : arr) {
             res += i;
         }
         System.out.println(res);
-        return res;
+
     }
     }
 
