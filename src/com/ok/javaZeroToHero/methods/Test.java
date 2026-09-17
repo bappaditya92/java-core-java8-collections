35ccf6e011e334a5a286054f115eb600cbfdd48f
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 21:04:19 +0530

    Add isPrime code

--- src/com/ok/javaZeroToHero/methods/Test.java
+++ src/com/ok/javaZeroToHero/methods/Test.java
@@ -3,15 +3,27 @@ package com.ok.javaZeroToHero.methods;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(sum(10,52,66,96,88));
+
+
+        System.out.println(isPrime(17));
+        System.out.println(isPrime(20));
+        System.out.println(isPrime(19));
     }
 
-    public static int sum(int ...num){
+    public static boolean isPrime(int num){
         int res = 0;
-        for(int i: num){
-            res+= i;
+
+        for(int i=1; i<num/2; i++){
+            if(num %i == 0){
+                res++;
+            }
+
         }
-        return res;
+
+        return res == 1;
+
     }
+
+
     }
 
