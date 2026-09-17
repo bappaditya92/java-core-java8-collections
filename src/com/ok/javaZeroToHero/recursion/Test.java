db4af85da86274e7ebef85467ef016456a979196
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-17 21:59:01 +0530

    Add recursion codes

--- src/com/ok/javaZeroToHero/recursion/Test.java
+++ src/com/ok/javaZeroToHero/recursion/Test.java
@@ -2,14 +2,28 @@ package com.ok.javaZeroToHero.recursion;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+       // System.out.println(factorial(5));
+
+        //5 + 4 + 3 + 2 + 1
+
+        System.out.println(sumOfNaturalNumbers(5));
 
     }
-    public static int factorial(int x){
-        if(x==1){
-            return 1;
+   // public static int factorial(int x){
+//        if(x==1){
+//            return 1;
+//        }
+//        return x *factorial(x-1);
+
+  //  }
+
+    public static int sumOfNaturalNumbers(int n) {
+        if (n == 0) {
+            return 0;
         }
-        return x *factorial(x-1);
+            return n + sumOfNaturalNumbers(n - 1);
 
     }
+
+
 }
