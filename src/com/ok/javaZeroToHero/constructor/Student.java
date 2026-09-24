014631f2b180d8298632a4b0126f4ecd9984338a
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-24 13:01:13 +0530

    Add some code of constructor

---
+++ src/com/ok/javaZeroToHero/constructor/Student.java
@@ -0,0 +1,33 @@
+package com.ok.javaZeroToHero.constructor;
+
+public class Student {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    private String name;
+        private int age;
+        private int rollNo;
+}
+
---
+++ src/com/ok/javaZeroToHero/constructor/Test.java
@@ -0,0 +1,9 @@
+package com.ok.javaZeroToHero.constructor;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(20);
+        System.out.println(student.getAge());
+    }
+}
