3f8358af01bef5e3f3d40c0a174dacf22610d31b
bappaditya92 <bappadityasinha.92@gmail.com>
2026-09-26 13:45:47 +0530

    add constructor practice code

--- src/com/ok/javaZeroToHero/constructor/Student.java
+++ src/com/ok/javaZeroToHero/constructor/Student.java
@@ -2,6 +2,21 @@ package com.ok.javaZeroToHero.constructor;
 
 public class Student {
 
+
+
+    public String name;
+        private int age;
+        private int rollNo;
+
+        public Student(String name, int age, int rollNo) {
+            this.age = age;
+            this.name = name;
+            this.rollNo = rollNo;
+        }
+
+
+
+
     public String getName() {
         return name;
     }
@@ -25,9 +40,5 @@ public class Student {
     public void setRollNo(int rollNo) {
         this.rollNo = rollNo;
     }
-
-    private String name;
-        private int age;
-        private int rollNo;
 }
 
--- src/com/ok/javaZeroToHero/constructor/Test.java
+++ src/com/ok/javaZeroToHero/constructor/Test.java
@@ -2,8 +2,13 @@ package com.ok.javaZeroToHero.constructor;
 
 public class Test {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setAge(20);
+        Student student = new Student("Bappaditya", 20, 123);
+//        student.setAge(20);
+//        System.out.println(student.getAge());
+        //System.out.println(student.getAge());
+        System.out.println(student.name);
         System.out.println(student.getAge());
+        System.out.println(student.getRollNo());
+
     }
 }
